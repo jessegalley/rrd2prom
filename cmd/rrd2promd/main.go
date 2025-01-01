684038f1eafc1f0cfc51cf4111a8c2169634c5ee
jesse galley <jesse.galley@gmail.com>
2025-01-01 08:05:00 -0400

    updates to WIP main.go for testing purposes

--- cmd/rrd2promd/main.go
+++ cmd/rrd2promd/main.go
@@ -3,34 +3,87 @@ package main
 import (
 	"fmt"
 	"time"
+  "log"
+  "flag"
+  "os"
 
-	// "github.com/davecgh/go-spew/spew"
 	"github.com/jessegalley/rrd2prom"
 )
 
-
 func main() {
+    var (
+        rrdURL = flag.String("url", "", "URL or path of the RRD file to monitor")
+        name   = flag.String("name", "default", "Name identifier for the RRD metrics")
+    )
+    
+    flag.Parse()
 
-  url := ""
-  // rrdFile, err := rrd2prom.NewRRDFile(url, "port1")
-  // if err != nil {
-  //   fmt.Printf("couldn't open rrd file at: %s (%v)", url, err)
-  // }
+    if *rrdURL == "" {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-  // spew.Dump(rrdFile)
-
-  
-  for {
-    rrdFile, err := rrd2prom.NewRRDFile(url, "port1")
+    rrdFile, err := rrd2prom.NewRRDFile(*rrdURL, *name)
     if err != nil {
-      fmt.Printf("couldn't open rrd file at: %s (%v)", url, err)
+        log.Fatalf("couldn't open rrd file at: %s (%v)", *rrdURL, err)
     }
-    for dsname, v := range rrdFile.DataSources {
-      fmt.Printf("%s\t%s: %d\n", rrdFile.Name, dsname, v.LastValue)
+
+    for {
+        if err := rrdFile.Update(); err != nil {
+            log.Printf("couldn't update RRD file: %v\n", err)
+            continue
+        }
+
+        for dsname, v := range rrdFile.DataSources {
+            fmt.Printf("%s\t%s: %d\n", rrdFile.Name, dsname, v.LastValue)
+        }
+        
+        time.Sleep(rrdFile.Interval)
     }
-    time.Sleep(rrdFile.Interval)
-     
-  }
- 
 }
+// func main() {
+//
+//   // url := ""
+//   // rrdFile, err := rrd2prom.NewRRDFile(url, "port1")
+//   // if err != nil {
+//   //   fmt.Printf("couldn't open rrd file at: %s (%v)", url, err)
+//   // }
+//
+//   // spew.Dump(rrdFile)
+//   url := "https://cacti.net.hostopia.com/rra/leaf1h_mi2_traffic_in_44125.rrd"
+//
+//   // Initial creation
+//   rrdFile, err := rrd2prom.NewRRDFile(url, "port1")
+//   if err != nil {
+//     fmt.Printf("couldn't open rrd file at: %s (%v)", url, err)
+//     return
+//   }
+//
+//   for {
+//     // Update existing object
+//     if err := rrdFile.Update(); err != nil {
+//       fmt.Printf("couldn't update RRD file: %v\n", err)
+//       continue
+//     }
+//
+//     for dsname, v := range rrdFile.DataSources {
+//       fmt.Printf("%s\t%s: %d\n", rrdFile.Name, dsname, v.LastValue)
+//     }
+//
+//     time.Sleep(rrdFile.Interval)
+//   }
+//
+//   // for {
+//   //   rrdFile, err := rrd2prom.NewRRDFile(url, "port1")
+//   //   if err != nil {
+//   //     fmt.Printf("couldn't open rrd file at: %s (%v)", url, err)
+//   //   }
+//   //   for dsname, v := range rrdFile.DataSources {
+//   //     fmt.Printf("%s\t%s: %d\n", rrdFile.Name, dsname, v.LastValue)
+//   //   }
+//   //   time.Sleep(rrdFile.Interval)
+//   //    
+//   // }
+//  
+// }
 
